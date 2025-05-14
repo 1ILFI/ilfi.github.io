83959b7cedbccd498fdeac8592e0b8b304c63613
IFiori <115729425+1ILFI@users.noreply.github.com>
2025-05-14 13:52:43 +0800

    Delete iBlazorWebAssembly directory

--- iBlazorWebAssembly/App.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace iBlazorWebAssembly
-{
-    public partial class App : ComponentBase
-    {
-        // 这个类用于显式定义 App 组件，使其可以被 Program.cs 引用
-    }
-}
